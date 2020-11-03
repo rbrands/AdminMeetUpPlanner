e8216c74e85b72f1d882036c6d7f7750ded2aa32
rbrands <mail@robert-brands.com>
2020-11-03 07:30:30 +0100

    Startup configuration for Functions

---
+++ Shared/TenantSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace BlazorApp.Shared
+{
+    public class TenantSettings : CosmosDBEntity
+    {
+        [Required(ErrorMessage = "Bitte Bezeichnung für den Tenant eingeben"), MaxLength(180, ErrorMessage = "Tenantbezeichnung bitte kürzer als 180 Zeichen.")]
+        public string TenantName { get; set; }
+        [MaxLength(80, ErrorMessage = "Tenant-Key bitte kürzer als 80 Zeichen.")]
+        public string TenantKey { get; set; }
+        [Required(ErrorMessage = "Primary URL erforderlich."), MaxLength(180, ErrorMessage = "Primary URL bitte kürzer als 180 Zeichen.")]
+        public string PrimaryUrl { get; set; }
+        public List<string> AlternativeUrls { get; set; }
+        public Boolean ClubMemberShipAllow { get; set; } = true;
+        public Boolean GuestNameShown { get; set; } = true;
+    }
+}
