b3da537ad19cab40fa562f7569513fcb5a3f9c8f
rbrands <mail@robert-brands.com>
2020-11-07 19:41:16 +0100

    Introduction role tenantadmin and specific roles for every tenant

--- Shared/TenantSettings.cs
+++ Shared/TenantSettings.cs
@@ -12,6 +12,9 @@ namespace BlazorApp.Shared
         public string TenantName { get; set; }
         [MaxLength(80, ErrorMessage = "Tenant-Key bitte kürzer als 80 Zeichen.")]
         public string TenantKey { get; set; }
+        [Required(ErrorMessage = "Bitte Admin-Rolle angeben"), MaxLength(80, ErrorMessage = "Admin-Rolle bitte kürzer als 80 Zeichen.")]
+        [RegularExpression("[a-zA-Z0-9-_]*", ErrorMessage = "Bitte nur Buchstaben und Zahlen für Admin-Rolle.")]
+        public string AdminRole { get; set; }
         [Required(ErrorMessage = "Primary URL erforderlich."), MaxLength(180, ErrorMessage = "Primary URL bitte kürzer als 180 Zeichen.")]
         [RegularExpression(@"^https\:\/\/[0-9a-z]([-.\w]*[0-9a-z])*(:(0-9)*)*(\/?)([a-z0-9\-\.\?\,\'\/\\\+&%\$#_]*)?$", ErrorMessage = "Bitte eine gültige URL https://ausfahrten ... eingeben")]
         public string PrimaryUrl { get; set; }
@@ -20,5 +23,6 @@ namespace BlazorApp.Shared
         public Boolean IsLocked { get; set; } = false;
         public string LockMessage { get; set; }
         public Boolean TracksEnabled { get; set; } = true;
+        public Boolean LocalAdministrationDisabled { get; set; } = false;
     }
 }
