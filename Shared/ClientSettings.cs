c914c4cd9edbba3aa8cc9cb8a62cf89024215f1f
rbrands <mail@robert-brands.com>
2021-11-05 07:34:15 +0100

    Null value

--- Shared/ClientSettings.cs
+++ Shared/ClientSettings.cs
@@ -56,6 +56,7 @@ namespace BlazorApp.Shared
         public string Disclaimer { get; set; }
         [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string GuestDisclaimer { get; set; }
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
         public string RouteTagForQuickSelection {get; set; }
 
         public int GetMaxGroupSize(bool isAdmin)
